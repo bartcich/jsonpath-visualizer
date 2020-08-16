75d5350eb9429fb74a7db24ca4d85abb431611c1
Bartek Cichocki <bartek@codinarium.com>
2020-08-16 17:43:35 +0200

    Tree expanding and collapsing

--- src/components/tree/tree-node.tsx
+++ src/components/tree/tree-node.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FlattenedTreeNode } from "../../types/flattened-tree";
+import { useStore } from "../../store";
 
 const DEPTH_INDENT = 30;
 
@@ -11,10 +12,17 @@ interface TreeNodeProps {
 export const TreeNode: React.FC<TreeNodeProps> = (props) => {
   const { node, position } = props;
 
+  const store = useStore();
+
   const depth = (node.path.length - 1) * DEPTH_INDENT;
+
+  const handleCollapseTrigger = (): void => {
+    store.toggleCollapse(node);
+  };
 
   return (
     <div className="tree-node" style={{ top: position, marginLeft: depth }}>
+      {node.collapsible && <span onClick={handleCollapseTrigger}>{node.collapsed ? "[] " : "[x] "}</span>}
       {node.displayValue}
     </div>
   );
--- src/store/vis-store.ts
+++ src/store/vis-store.ts
@@ -1,24 +1,46 @@
 import { observable, action, computed } from "mobx";
+import jsonpath from "jsonpath";
 import { FlattenedTreeNode, TreePath } from "../types/flattened-tree";
-import { isArray, isPrimitive } from "../utils";
+import { isArray, isPrimitive, isObject } from "../utils";
 
 const ROW_HEIGHT = 20;
 const OVERFLOW_DISPLAY = 10;
 
-const constructFlattenedTreeNode = (node: any, path: TreePath): FlattenedTreeNode => {
+const nodeLabel = (node: any, path: TreePath, collapsed: boolean): string => {
   const key = path[path.length - 1];
-
   const isInObject = typeof key === "string";
 
-  // for array root (key is number) -> display nothing
-  // for object root -> display key name
-  // for primitive -> display `key: value`
-  const displayValue = `${isInObject ? `${key}: ` : ""}${isPrimitive(node) ? node : ""}`;
+  if (collapsed) {
+    if (isArray(node)) {
+      return `[Array(${node.length})]`;
+    } else if (isObject(node)) {
+      const objectKeys = Object.keys(node);
+      if (objectKeys.length < 1) {
+        return `{}`;
+      }
+      const firstKey = objectKeys[0];
+      if (isPrimitive(node[firstKey])) {
+        const hasMoreKeys = objectKeys.length > 1;
+        return `{ ${firstKey}: ${node[firstKey]} ${hasMoreKeys ? ", ..." : ""}}`;
+      }
+
+      return `[Object]`;
+    }
+  } else {
+    // for array root (key is number) -> display nothing
+    // for object root -> display key name
+    // for primitive -> display `key: value`
+    return `${isInObject ? `${key}: ` : ""}${isPrimitive(node) ? node : ""}`;
+  }
+};
+
+const constructFlattenedTreeNode = (node: any, path: TreePath, collapsed = false): FlattenedTreeNode => {
+  const displayValue = nodeLabel(node, path, collapsed);
   return {
     path,
     displayValue,
     collapsible: !isPrimitive(node),
-    collapsed: false,
+    collapsed,
   };
 };
 
@@ -43,17 +65,44 @@ const flattenTreeNode = (node: any, path: TreePath = ["$"]): FlattenedTreeNode[]
   }
 };
 
+const isPathDescendant = (parent: TreePath, obj: TreePath): boolean => {
+  if (obj.length < parent.length) {
+    return false;
+  }
+
+  return parent.every((v, i) => v === obj[i]);
+};
+
+const findNodePosition = (tree: FlattenedTreeNode[], node: FlattenedTreeNode): number => {
+  return tree.indexOf(node);
+};
+const findChildrenPostionRange = (tree: FlattenedTreeNode[], node: FlattenedTreeNode): [number, number] => {
+  const nodePosition = findNodePosition(tree, node);
+  for (let i = nodePosition + 1; i < tree.length; i++) {
+    if (!isPathDescendant(node.path, tree[i].path)) {
+      return [nodePosition, i];
+    }
+  }
+
+  return [nodePosition, tree.length];
+};
+
+const getJsonNodeByPath = (json: any, path: TreePath): any => {
+  return jsonpath.query(json, jsonpath.stringify(path), 1)[0];
+};
+
 export class VisStore {
-  @observable public jsonTree: any;
+  public jsonTree: any;
   @observable public viewportHeight: number;
   @observable public scrollPosition = 0;
+  public readonly flattenedTree = observable<FlattenedTreeNode>([]);
 
   importJson = (jsonString: string): void => {
     try {
-      console.info("importing");
       const json = JSON.parse(jsonString);
       this.jsonTree = json;
-      console.info("import done");
+
+      this.flattenedTree.replace(flattenTreeNode(this.jsonTree));
     } catch (e) {
       console.error(e);
     }
@@ -67,6 +116,20 @@ export class VisStore {
   @action
   setScrollPosition = (pos: number): void => {
     this.scrollPosition = pos;
+  };
+
+  @action
+  toggleCollapse = (node: FlattenedTreeNode): void => {
+    const jsonElement = getJsonNodeByPath(this.jsonTree, node.path);
+    const newNode = constructFlattenedTreeNode(jsonElement, node.path, !node.collapsed);
+    if (!node.collapsed) {
+      const [from, to] = findChildrenPostionRange(this.flattenedTree, node);
+      this.flattenedTree.splice(from, to - from, newNode);
+    } else {
+      const from = findNodePosition(this.flattenedTree, node);
+      const newNodes = [...flattenTreeNode(jsonElement, node.path)];
+      this.flattenedTree.splice(from, 1, ...newNodes);
+    }
   };
 
   @computed get displayContainerHeight(): number {
@@ -87,12 +150,5 @@ export class VisStore {
       this.displayStartingIndex + this.displayItemsCount,
     );
     return displayTree;
-  }
-
-  @computed get flattenedTree(): FlattenedTreeNode[] {
-    console.info("flattening");
-    const result = this.jsonTree ? flattenTreeNode(this.jsonTree) : null;
-    console.info("flattening done");
-    return result;
   }
 }
